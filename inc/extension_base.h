6d76160c7f7db155aeebcb7ec2a841c6b8760383
Ameya Vikram Singh <ameya.v.singh@gmail.com>
2023-04-10 20:05:40 +0530

    Fix Build issue
    * Update spdlog git submodule
    
    * Fix Compilation failure for missing parameter
    
    * Update CMakeLists.txt add dependency on SystemC, spdlog, and Boost library using Environment variables

--- inc/extension_base.h
+++ inc/extension_base.h
@@ -82,7 +82,7 @@ namespace riscv_tlm {
         }
 
         bool NOP() {
-            logger->debug("{} ns. PC: 0x{:x}. NOP! new PC 0x{:x} ", sc_core::sc_time_stamp().value(), regs->getPC());
+            logger->debug("{} ns. PC: 0x{:x}. NOP! new PC 0x{:x} ", sc_core::sc_time_stamp().value(), regs->getPC(), regs->getCSR(CSR_MTVEC));
             logger->flush();
             sc_core::sc_stop();
             return true;
