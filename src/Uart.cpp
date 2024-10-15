47efc3a70e9357b10ccd306ec51319517930cb1a
Màrius Montón <monton@ieec.cat>
2024-10-15 11:50:08 +0200

    use offset address values

--- src/Uart.cpp
+++ src/Uart.cpp
@@ -3,6 +3,9 @@
  \brief Basic TLM-2 UART module
  \author Màrius Montón
  \date May 2024
+
+Based on https://github.com/lowRISC/ibex-demo-system/blob/main/rtl/system/uart.sv model
+
  */
 // SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -24,7 +27,7 @@ void Uart::b_transport(tlm::tlm_generic_payload &trans,
                             sc_core::sc_time &delay) {
 
         tlm::tlm_command cmd = trans.get_command();
-        sc_dt::uint64 addr = trans.get_address();
+        sc_dt::uint64 addr = trans.get_address() - IBEX_UART_MEMORY_ADDRESS;
         unsigned char *ptr = trans.get_data_ptr();
         unsigned int len = trans.get_data_length();
         delay = sc_core::SC_ZERO_TIME;
@@ -35,37 +38,29 @@ void Uart::b_transport(tlm::tlm_generic_payload &trans,
         if (cmd == tlm::TLM_WRITE_COMMAND) {
             memcpy(&aux_value, ptr, len);
             switch (addr) {
-                case TIMER_MEMORY_ADDRESS_LO:
-                   
+                case IBEX_UART_RX_REG:
                     break;
-                case TIMER_MEMORY_ADDRESS_HI:
-                    
+                case IBEX_UART_TX_REG:
+                    std::cout << aux_value << std::flush;
                     break;
-                case TIMERCMP_MEMORY_ADDRESS_LO:
-                    
-                    break;
-                case TIMERCMP_MEMORY_ADDRESS_HI:
-                                      
+                case IBEX_UART_STATUS_REG:
                     uart_event.notify(sc_core::sc_time(notify_time, sc_core::SC_NS));
-                    break;
+                    break;                            
+                
                 default:
                     trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
                     return;
             }
         } else { // TLM_READ_COMMAND
             switch (addr) {
-                case TIMER_MEMORY_ADDRESS_LO:
-                    
-                    
+                case IBEX_UART_RX_REG:
+                    aux_value = 0;
                     break;
-                case TIMER_MEMORY_ADDRESS_HI:
-                    
+                case IBEX_UART_TX_REG:
+                    aux_value = 0;
                     break;
-                case TIMERCMP_MEMORY_ADDRESS_LO:
-                    
-                    break;
-                case TIMERCMP_MEMORY_ADDRESS_HI:
-                    
+                case IBEX_UART_STATUS_REG:
+                    aux_value = UartStatusReg;
                     break;
                 default:
                     trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
