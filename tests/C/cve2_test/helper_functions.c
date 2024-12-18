e1f32a7724fe38ec99881c12385031cf2fd3812a
Màrius Montón <marius.monton@gmail.com>
2024-12-18 14:21:17 +0100

    test example for CVE2

---
+++ tests/C/cve2_test/helper_functions.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <stdio.h>
+
+#define TRACE (*(unsigned char *)0x80001000)
+
+int _read(int file, char* ptr, int len) {
+    return 0;
+}
+
+int _close(int fd){
+    return 0;
+}
+
+int _fstat_r(int fd) {
+    return 0;
+}
+
+int _lseek_r(struct _reent *ptr, FILE *fp, long offset, int whence){
+    return 0;
+}
+
+int _isatty_r(struct _reent *ptr, int fd) {
+    return 0;
+}
+
+int _getpid(void) {
+    return 0;
+}
+
+void _kill(int pid) {
+
+}
+
+int _write(int file, const char *ptr, int len) {
+  int x;
+
+  for (x = 0; x < len; x++) {
+    TRACE =  *ptr++;
+  }
+
+  return (len);
+}
+
---
+++ tests/C/cve2_test/trace.c
@@ -0,0 +1,11 @@
+
+#define TRACE (*(unsigned char *)0x40000000)
+
+int main(void) {
+  TRACE = 'H';
+  TRACE = 'e';
+
+  asm volatile ("fence");
+  asm volatile ("ecall");
+
+}
