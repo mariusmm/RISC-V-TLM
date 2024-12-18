48ffb917414c1cdf588573cea47918e146bee7b6
Màrius Montón <marius.monton@gmail.com>
2024-12-18 16:23:56 +0100

    remove useless export

--- compile_spdlog.sh
+++ compile_spdlog.sh
@@ -3,4 +3,3 @@ cd spdlog
 cmake -H. -B_builds -DCMAKE_INSTALL_PREFIX=install -DCMAKE_BUILD_TYPE=Release
 cmake --build _builds --config Release
 cmake --build _builds --target install
-export SPDLOG_HOME=$PWD/install
